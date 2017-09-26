993cc0f1bf3ee7897d0e1c1abdc5b7d1c9f4635b
Kevin Lin <khlin@buffalo.edu>
2017-09-26 13:55:57 -0400

    Initial Commit

---
+++ Python/ACMWorkshop.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+age = 17
+string_num = "9"
+print(int(string_num))
+print("Type in quotation marks \" \' by using \ !")
+grade = 13
+college_student = grade > 12
+profile = ["name", "bio", age, grade]
+print(profile)
+fav_nums = [0, 1, 2, 7, 8, 9, 10]
+fav_nums[1] = 2
+my_dictionary = {
+    0: "Zero Escape",
+    1: "We Are Number One",
+    2: "Sheik fair noise",
+    7: "Lucky seven",
+    8: "Chinese lucky number",
+    9: "Nonary Game",
+    10: "10/10 Duwangs"
+}
+print(my_dictionary)
+print(my_dictionary[10])
