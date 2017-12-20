1d5ba7027f7ca0e79d29e327de789ec543863a4d
Kevin Lin <klin7743@bths.edu>
2017-12-19 23:37:59 -0500

    Basic ListView example

---
+++ AndroidStudio/ListViewExample/app/src/main/java/com/example/khlin/listviewexample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.khlin.listviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] favoriteTVShows = {"The Walking Dead", "Game of Thrones", "Breaking Bad", "House"};
+
+        ListAdapter theAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, favoriteTVShows);
+
+        ListView theListView = (ListView) findViewById(R.id.theListView);
+
+        theListView.setAdapter(theAdapter);
+
+        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String tvShowPicked = "You selected " + String.valueOf(adapterView.getItemAtPosition(position));
+
+                Toast.makeText(MainActivity.this, tvShowPicked, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
